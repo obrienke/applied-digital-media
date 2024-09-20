fb5ca7630bd51d9ccca4ac6a4a0f899e7720628f
obrienke <kevin.obrien@gmit.ie>
2024-09-20 14:32:22 +0000

    code from lab 1

---
+++ lab1/ex4.js
@@ -0,0 +1,19 @@
+function clickHandler(){
+    alert('Button Clicked');
+}
+
+let name = "Kevin";
+let number = 100;
+number = "One Hundred";
+
+number = 2;
+if(number == 3){
+    alert("Number is valid")
+}else{
+    alert("Number is not valid")
+}
+
+for(let i = 1; i <= 5; i++){
+    document.write("<br> Loop " + i);
+}
+
